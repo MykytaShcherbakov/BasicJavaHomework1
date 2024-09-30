af3679874144907fcc0609016565e6badc5d4bdb
Mykyta <shcherbakov.mykyta.s@gmail.com>
2024-09-30 13:05:06 +0200

    Homework1

---
+++ src/HM1.java
@@ -0,0 +1,39 @@
+public class HM1 {
+    public static void main(String[] args) {
+//        char myChar = 'G';
+//        int myInt = 89;
+//        byte myByte = 4;
+//        short myShort = 56;
+//        float myFloat = 4.7333436f;
+//        double myDouble = 4.355453532;
+//        long myLong = 12121L;
+//
+//        System.out.println("char: " + myChar);
+//        System.out.println("int: " + myInt);
+//        System.out.println("byte: " + myByte);
+//        System.out.println("short: " + myShort);
+//        System.out.println("float: " + myFloat);
+//        System.out.println("double: " + myDouble);
+//        System.out.println("long: " + myLong);
+
+        char myChar = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436f;
+        double myDouble = 4.355453532;
+        long myLong = 12121L;
+
+        System.out.println("Character: " + myChar);
+        System.out.println("Integer: " + myInt);
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Long: " + myLong);
+
+
+
+
+    }
+}
